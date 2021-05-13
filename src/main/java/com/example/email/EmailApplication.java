c0bda09ab93d31247a2cbfd32de187687dfbd8b0
NantipatSoften <keymasterviriya1150@gmail.com>
2021-05-13 15:07:12 +0700

    🎉 initial commit

--- src/main/java/com/example/email/EmailApplication.java
+++ src/main/java/com/example/email/EmailApplication.java
@@ -1,4 +1,4 @@
-package email;
+package com.example.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmailApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
 
 }
