ae1c917324bb0cb49184d31570d73a5e596dee65
NantipatSoften <keymasterviriya1150@gmail.com>
2021-05-13 17:11:43 +0700

    conf

---
+++ src/main/java/com/example/email/listener/EmailListener.java
@@ -0,0 +1,14 @@
+package com.example.email.listener;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class EmailListener {
+    @KafkaListener(topics = "activation-email")
+    public void listenForActivationEmail(String message){
+        log.info("Kafka received: " + message);
+    }
+}
